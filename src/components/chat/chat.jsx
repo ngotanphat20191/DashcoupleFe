f41a293ad302cb117648fabfef3e20fad69369b9
ngotanphat20191 <ngotanphat02032003@gmail.com>
2025-04-20 02:40:44 +0700

    commit lần 8

--- src/components/chat/chat.jsx
+++ src/components/chat/chat.jsx
@@ -85,7 +85,7 @@ const Chat = () => {
         })
     };
     useEffect(() => {
-        const socket = new SockJS('wss://present-ghastly-puma.ngrok-free.app/api/websocket');
+        const socket = new SockJS('https://present-ghastly-puma.ngrok-free.app/api/websocket');
         const client = new Client({
             webSocketFactory: () => socket,
             reconnectDelay: 5000,
